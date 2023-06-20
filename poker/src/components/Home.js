8615973bafa40d449a6ed94d7d7ee2b342db5aed
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-06-19 21:59:47 -0400

    save before manipulating for table sim

--- poker/src/components/Home.js
+++ poker/src/components/Home.js
@@ -1,37 +1,35 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import Login from "./Login";
 import Footer from "./Footer";
 
 const Home = () => {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
-
   const userInfo = useSelector(state => state.auth.user);
 
+  const pokerImageURL = "https://via.placeholder.com/150"; // replace with your image URL
+
   return (
-    <div className="App">
-      <div className="foground container">
-        <div className="row">
-          <div className="col">
-            <div className="p-3 border bg-light">
-              <h1>Welcome To Poker</h1>
-              <p>A Poker Platform </p>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <div className="p-3 border bg-light">
+    <Container className="App">
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <Card className="mb-4 shadow-sm">
+            <Card.Img variant="top" src={pokerImageURL} />
+            <Card.Body>
+              <Card.Title>Welcome To Poker</Card.Title>
+              <Card.Text>
+                A Poker Platform
+              </Card.Text>
               {!isAuthenticated && <Login /> }
-              {isAuthenticated && <h2>Welcome Back {userInfo.username}!</h2>}
-            </div>
-          </div>
-        </div>
-      </div>
+              {isAuthenticated && <Card.Text>Welcome back, {userInfo.username}!</Card.Text>}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
       <Footer />
-    </div>
+    </Container>
   );
 };
-
 
 export default Home;
