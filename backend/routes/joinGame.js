9cf67a40a9c31234f920c5c35512b752efb43da6
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-07-21 10:54:31 -0400

    able to join and leave table of choice and it is reflected accurately

--- backend/routes/joinGame.js
+++ backend/routes/joinGame.js
@@ -4,8 +4,37 @@ const User = require("../models/userSchema");
 
 router.post('/join/:gameId/:seatId', async (req, res) => {    
   try {
-    // ... (existing code)
+    console.log("Request params: ", req.params);
+    console.log("Request body: ", req.body);
+    const { userId, buyIn } = req.body;
+    console.log("userId: ", userId);
 
+    const game = await Game.findById(req.params.gameId);
+    
+    if(!game) {
+      return res.status(404).json({ message: "Game not found!" });
+    }
+
+    const user = await User.findById(userId);
+    
+    if(!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    
+    if(user.accountBalance < buyIn) {
+      return res.status(400).json({ message: "Insufficient funds!" });
+    }
+
+    // Check if the user is already sitting in any seat of this game
+    const alreadySitting = game.seats.some(seat => seat.player && seat.player.user.toString() === userId);
+
+    if(alreadySitting) {
+      return res.status(400).json({ message: "You are already sitting at a seat in this game!" });
+    }
+
+    const { seatId } = req.params;
+    console.log('seatId:', seatId);
+    
     // Convert seatId and seat._id to strings for comparison
     const availableSeat = game.seats.find(seat => seat._id.toString() === seatId && seat.player === null);
     console.log('availableSeat:', availableSeat);
