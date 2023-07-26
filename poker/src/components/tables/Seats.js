e0923aebbb3eeb93f08f5d094ea9528035323d8a
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-07-26 12:44:52 -0400

    still trying to fix sit here

--- poker/src/components/tables/Seats.js
+++ poker/src/components/tables/Seats.js
@@ -1,25 +1,24 @@
-import { Container, Row, Col, Button, Card } from "react-bootstrap";
-import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Slider from "react-input-slider";
-
 import { joinGame } from "../../rtk/slices/serverSlice";
+import { Button } from "react-bootstrap";
 
 const Seat = ({ seat }) => {
   const user = useSelector((state) => state.auth.user);
   const {tableId} = useParams();
   const seatId = seat._id;
-
   const { viewedGame } = useSelector((state) => state.server);
-
   const maxBuyIn = viewedGame.game.max;
-  const minBuyIn = viewedGame.game.min;
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(joinGame(tableId, seatId, user.id, maxBuyIn));
+  };
 
   return (
     <div className="d-flex justify-content-center seat">
       <p>{seat && `Seat ${seat.id}`}</p>
-      {seat && !seat.player && <Button className="sit-here">Sit here</Button>}
+      {seat && !seat.player && <Button className="sit-here" onClick={handleClick}>Sit here</Button>}
     </div>
   );
 };
