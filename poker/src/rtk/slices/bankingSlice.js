c8c8fb41aebd24f477cb52da6d747a81d7f40464
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-07-28 10:00:19 -0400

    able to get deposit and with draw work but having hard time with getting everything to wrok together

---
+++ poker/src/rtk/slices/bankingSlice.js
@@ -0,0 +1,26 @@
+import {createAsyncThunk, createAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const depositSuccess = createAction('banking/depositSuccess');
+export const withdrawSuccess = createAction('banking/withdrawSuccess');
+
+export const deposit = createAsyncThunk(
+  "banking/deposit",
+  async ({ userId, amount }, { dispatch }) => {
+    console.log('userId', userId);
+    const response = await axios.post("http://localhost:4000/deposit", { userId, amount });
+    console.log(response.data);
+    dispatch(depositSuccess(response.data));
+    return response.data;
+  }
+);
+
+export const withdraw = createAsyncThunk(
+  "banking/withdraw",
+  async ({ userId, amount }, { dispatch }) => {
+    const response = await axios.post("http://localhost:4000/withdraw", { userId, amount });
+    console.log(response.data);
+    dispatch(withdrawSuccess(response.data));
+    return response.data;
+  }
+);
