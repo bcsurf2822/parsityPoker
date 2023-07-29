deb52bc7ee46d05056794daf54e65ed88a852858
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-07-29 11:05:27 -0400

    starting to set up schema for chat and rest  of app fixed up a bit

---
+++ backend/models/chatSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ChatSchema = new Schema({
+  participants: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  messages: [MessageSchema],
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+const Chat = mongoose.model("Chat", ChatSchema);
+
+module.exports = {
+  Message,
+  Chat
+};
