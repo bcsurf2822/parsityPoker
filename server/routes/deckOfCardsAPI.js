3534c7ff2e1352282ae79727df228e7efb176c2b
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-08-08 13:07:11 -0400

    got blinds to work and added deckOfCardAPI route

---
+++ server/routes/deckOfCardsAPI.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const axios = require("axios");
+
+//https://www.deckofcardsapi.com/
+
+//Deck and Number of Decks
+
+// Draw a card
+
+//Shuffle the deck
+
+//NewDeck in order and jokers          jokers_enabled=true
+
+//Partial Deck
+
+//Adding to Piles (not with multiple decks)
+
+//shuffle the pile
+
+//List Cards In Piles
+
+//Back of Card Image
