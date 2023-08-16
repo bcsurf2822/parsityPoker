549289fbd99017e7bc137b6a8d96da527f09f742
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-08-16 11:53:14 -0400

    able to get winner seat id

--- server/routes/pokerAPI.js
+++ server/routes/pokerAPI.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const axios = require("axios");
 const Game = require("../models/gamesSchema");
+
 router.get("/winner/:gameId", async (req, res) => {
   const { gameId } = req.params;
 
@@ -15,19 +16,26 @@ router.get("/winner/:gameId", async (req, res) => {
 
     const communityCards = game.communityCards.join(',');
 
-    const playerCards = game.seats
-      .filter(seat => seat.player && seat.player.handCards.length)
-      .map(seat => seat.player.handCards.join(','))
-      .map(cards => `pc[]=${cards}`)
-      .join('&');
+    // Step 1: Build the playerCards array.
+    const occupiedSeats = game.seats.filter(seat => seat.player && seat.player.handCards.length);
+    const playerCards = occupiedSeats.map(seat => seat.player.handCards.join(',')).map(cards => `pc[]=${cards}`).join('&');
 
     const url = `https://api.pokerapi.dev/v1/winner/texas_holdem?cc=${communityCards}&${playerCards}`;
 
     const response = await axios.get(url);
 
     if (response && response.data) {
-
       game.winnerData = response.data;
+
+      // Step 2: Use the returned players property to match the winner's cards.
+      const winningCards = response.data.winners[0].cards;
+      const winnerIndex = response.data.players.findIndex(player => player.cards === winningCards);
+
+      // Step 3: Match the winner's index to the occupied seats' index.
+      if (winnerIndex !== -1) {
+        const winningSeat = occupiedSeats[winnerIndex];
+        game.winnerData.winningSeatId = winningSeat._id;
+      }
 
       await game.save();
       req.io.emit("winner", game);
