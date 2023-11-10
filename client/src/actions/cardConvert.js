60289681e97a3a8f6f261a396c5b36d7f3f1a26b
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-11-10 12:21:23 -0500

    deleted un needed files because the new system works all ext api calls are made from backend and everything works as it did in promotions including the images of the cards

--- client/src/actions/cardConvert.js
+++
@@ -1,9 +0,0 @@
-export const convertRank = rank => {
-  const rankMapping = { 'A': 'A', 'K': 'K', 'Q': 'Q', 'J': 'J' };
-  return rankMapping[rank] || rank;
-};
-
-export const convertSuit = suit => {
-  const suitMapping = { 'clubs': 'C', 'diamonds': 'D', 'hearts': 'H', 'spades': 'S' };
-  return suitMapping[suit];
-};
--- client/src/actions/getCards.js
+++ client/src/actions/getCards.js
@@ -1,22 +1,16 @@
 export const getCards = (cardCode) => {
-
   const suitMap = {
-    'H': 'Hearts',
-    'D': 'Diamonds',
-    'C': 'Clubs',
-    'S': 'Spades'
+    H: "Hearts",
+    D: "Diamonds",
+    C: "Clubs",
+    S: "Spades",
   };
 
   const value = cardCode.substring(0, cardCode.length - 1);
 
-  console.log("GET CARD IMG VALUE", value)
-
-
   const suit = cardCode[cardCode.length - 1];
 
-  console.log("GET CARD IMG SUIT", suit)
   const imageName = `${value}${suitMap[suit]}.png`;
-  console.log("GET CARD IMG NAME", imageName)
 
   return `/deck/${imageName}`;
 };
