17af7ac6c2831ce711405d6aae3b6fab3901516f
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-11-13 14:30:37 -0500

    messing with animating number although it needs more work

--- client/src/components/tables/SeatDetails/UserNameAndChips.js
+++ client/src/components/tables/SeatDetails/UserNameAndChips.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 
-export default function UserNameAndChips({user, chipCount}) {
+export default function UserNameAndChips({ user, chipCount }) {
   return (
-    <Grid container alignItems="center">
-      <Grid item xs>
-        <Typography gutterBottom variant="h4" component="div">
-          {user}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Typography gutterBottom variant="h6" component="div">
-          {chipCount}
-        </Typography>
-      </Grid>
-    </Grid>
-  )
+    <div>
+      <Typography gutterBottom variant="h6" component="div" align="center">
+        ${chipCount}
+      </Typography>
+      <Typography gutterBottom variant="h4" component="div" align="center">
+        {user}
+      </Typography>
+    </div>
+  );
 }
