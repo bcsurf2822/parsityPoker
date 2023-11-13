74d77cfbc8e36cc4af6b3f9bd7aa0d954939e6a3
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-11-13 01:57:15 -0500

    new code is working well with props passed into the table

--- client/src/components/tables/TableDetails/Table.js
+++ client/src/components/tables/TableDetails/Table.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { getCards } from '../../../actions/getCards';
 
-export default function Table({ pot, cards}) {
+export default function Table({ pot, cards }) {
   return (
-    <div>
-      The Table
-    </div>
-  )
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      {/* Community Cards */}
+      <Box
+        display="flex"
+        justifyContent="space-around"
+        width="100%" // Adjust as necessary
+        mb={2} // Margin bottom
+      >
+        {cards.map((card, index) => (
+          <Box key={index} p={1} /* Padding */>
+            <img src={getCards(card)} alt={`Card ${card}`} />
+          </Box>
+        ))}
+      </Box>
+
+      {/* Pot Amount */}
+      <Typography variant="h6">
+        Pot: {pot}
+      </Typography>
+
+      {/* Placeholder for Chip Animation */}
+      <Box mt={2} /* Margin top */>
+        {/* Chip animation will go here */}
+      </Box>
+    </Box>
+  );
 }
