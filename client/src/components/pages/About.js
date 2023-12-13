2fe9345958cf56b88728c6ee05df3381ad4037cc
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-12-13 14:01:32 -0500

    will probably just use bootstrap for layout started in about section

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,51 +1,20 @@
 
-import Box from '@mui/joy/Box';
-import Stack from '@mui/joy/Stack';
-import { styled } from '@mui/system';
-
-const AboutChildren = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#262B32' : '#fff',
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  borderRadius: 4,
-  border: '1px solid',
-}));
-
-const Table = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#262B32' : '#fff',
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  borderRadius: 4,
-  border: '1px solid',
-  height: '400px',
-  width: '800px',
-}));
-
 
 const About = () => {
   return (
-    <div>
-          <Box sx={{
-      display: 'flex',        // Enables Flexbox
-      justifyContent: 'center', // Horizontally centers the content
-      alignItems: 'center',     // Vertically centers the content
-      height: '97vh',          // Sets the height of the box to full viewport height
-      border: '1px solid',
-    }}>
- <Stack
-  direction="row"
-  justifyContent="center"
-  alignItems="center"
-  spacing={4}
->
-        <AboutChildren>Item 1</AboutChildren>
-        <Table>Table</Table>
-        <AboutChildren>Item 2</AboutChildren>
-
-      </Stack>
-    </Box>
     
-    </div>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">col</div>
+    <div class="col">col</div>
+    <div class="col">col</div>
+    <div class="col">col</div>
+  </div>
+  <div class="row">
+    <div class="col-8">col-8</div>
+    <div class="col-4">col-4</div>
+  </div>
+</div>
   );
 };
 
