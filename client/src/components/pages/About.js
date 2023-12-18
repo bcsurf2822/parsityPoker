688a1aef973e40d89702b6b244ccc8a55580c761
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-12-18 11:35:06 -0500

    adjusted centered row and started using css for the about where i iwill use this design for the room and seats

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,41 +1,27 @@
-
-
 const About = () => {
   return (
-    <div className="container text-center">
+    <div
+      className="roomNew container text-center d-flex flex-column justify-content-center"
+      style={{ height: "100vh" }}
+    >
       <div className="row">
-        <div className="col bordered-col">col</div>
-        <div className="col bordered-col">col</div>
-        <div className="col bordered-col">col</div>
-        <div className="col bordered-col">col</div>
+        <div className="emptyLTop col bordered-col">Empy Left Top</div>
+        <div className="seatTop col bordered-col">Seat 1</div>
+        <div className="seatTop col bordered-col">Seat 2</div>
+        <div className="emptyRTop col bordered-col">Empty R Top</div>
+      </div>
+      <div className="table-row row ml-3">
+        <div className="seatMidL col-2 bordered-col">Seat 6</div>
+        <div className="tableMid col-6 bordered-col">Table</div>
+        <div className="seatMidR col-2 bordered-col">Seat 3</div>
       </div>
       <div className="row">
-      <div className="col-2 bordered-col">col-2</div>
-        <div className="col-6 bordered-col">col-8</div>
-        <div className="col-2 bordered-col">col-2</div>
+        <div className="emptyLBot col bordered-col">Empty L Bottom</div>
+        <div className="seatBot col bordered-col">Seat 4</div>
+        <div className="seatBot col bordered-col">Seat 5</div>
+        <div className="emptyRBot col bordered-col">Empty R Bottom</div>
       </div>
-      <div className="row">
-        <div className="col bordered-col">col</div>
-        <div className="col bordered-col">col</div>
-        <div className="col bordered-col">col</div>
-        <div className="col bordered-col">col</div>
-      </div>
-
-      <div class="container text-center">
-  <div class="row align-items-end">
-    <div class="col">
-      One of three columns
     </div>
-    <div class="col">
-      Chatbox area
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-  </div>
-</div>
-    </div>
-  
   );
 };
 
