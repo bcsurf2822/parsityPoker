429743dee9d2c2cd57968287aca1af610bb84de4
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-01-19 15:14:58 -0500

    able to open multiple windows at once now

--- client/src/components/NavBarWrapper.js
+++ client/src/components/NavBarWrapper.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import MyNav from "./Navbar";
-import { useSelector } from "react-redux";
 
 const NavbarWrapper = () => {
-
   const location = useLocation();
-  const isPokerRoom = location.pathname.startsWith('/room/');
+  const isPokerRoom = location.pathname.startsWith("/room/");
 
   return <>{!isPokerRoom && <MyNav />}</>;
 };
--- client/src/components/pages/OpenRoomButton.js
+++ client/src/components/pages/OpenRoomButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const OpenPokerRoomButton = ({gameId}) => {
+const OpenPokerRoomButton = ({ gameId }) => {
     const openPokerRoom = () => {
-        const pokerRoomUrl = `/room/${gameId}`; 
+        const pokerRoomUrl = `/room/${gameId}`;
         const windowFeatures = "menubar=no,toolbar=no,status=no,width=800,height=600";
-        window.open(pokerRoomUrl, 'PokerRoomWindow', windowFeatures);
+        // Use gameId to create a unique window name for each game
+        window.open(pokerRoomUrl, `PokerRoomWindow_${gameId}`, windowFeatures);
     };
 
     return (
