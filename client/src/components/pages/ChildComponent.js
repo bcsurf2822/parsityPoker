6d065fd53bd139f1eb4e6c1dfe0c2308ce200468
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-01-27 11:04:46 -0500

    the design for table and chairs seems to be working in sample room and childComp

--- client/src/components/pages/ChildComponent.js
+++ client/src/components/pages/ChildComponent.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-
 const ChildComponent = () => {
   return (
     <div className="layout">
-      
-      <div className="responsive-rectangle">Rectangle</div>
-
+      <div className="square-row top">
+        <div className="responsive-square">Square 1</div>
+        <div className="responsive-square">Square 2</div>
+      </div>
+      <div className="rectangle-row"> {/* Container for the middle row */}
+        <div className="responsive-square">Square 5</div> {/* Square to the left */}
+        <div className="responsive-rectangle">Rectangle</div>
+        <div className="responsive-square">Square 6</div> {/* Square to the right */}
+      </div>
+      <div className="square-row bottom">
+        <div className="responsive-square">Square 3</div>
+        <div className="responsive-square">Square 4</div>
+      </div>
     </div>
   );
 };
