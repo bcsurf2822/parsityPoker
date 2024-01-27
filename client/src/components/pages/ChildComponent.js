d099618ebf250043dc1c4cead489d0f646cccafc
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-01-27 10:56:35 -0500

    able to put and rect in a sq and make it responsive to page height

--- client/src/components/pages/ChildComponent.js
+++ client/src/components/pages/ChildComponent.js
@@ -4,13 +4,9 @@ import React from 'react';
 const ChildComponent = () => {
   return (
     <div className="layout">
-      <div className="square">1</div>
-      <div className="square">2</div>
-      <div className="square">3</div>
-      <div className="rectangle">Rectangle</div>
-      <div className="square">4</div>
-      <div className="square">5</div>
-      <div className="square">6</div>
+      
+      <div className="responsive-rectangle">Rectangle</div>
+
     </div>
   );
 };
--- client/src/components/tables/SampleRoom.js
+++ client/src/components/tables/SampleRoom.js
@@ -4,7 +4,7 @@ import ChildComponent from "../pages/ChildComponent";
 export default function SampleRoom() {
   return (
     <div className="responsive-box">
-
+      <ChildComponent />
   </div>
   );
 }
