bd46ebcccdf44449862b758d120f8227a7513237
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-02-01 15:42:58 -0500

    change the placeholder ot square still not complete

--- client/src/components/pages/RectangleContent.js
+++ client/src/components/pages/RectangleContent.js
@@ -4,9 +4,9 @@ import React from 'react';
 const RectangleContent = () => {
   return (
     <div className="rectangle-content">
-      <div className="placeholders">
+      <div className="card-slots">
         {[...Array(5)].map((_, index) => (
-          <div key={index} className="placeholder-square">P{index + 1}</div>
+          <div key={index} className="card-slot">Card {index + 1}</div>
         ))}
       </div>
       <div className="amount-display">$0.00</div>
