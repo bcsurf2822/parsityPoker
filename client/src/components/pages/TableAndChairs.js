c61476bf17e8d4011efdd2be489a967bf06d4bbe
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-08-12 09:40:58 -0400

    sit here button added on all seats and label seats

--- client/src/components/pages/TableAndChairs.js
+++ client/src/components/pages/TableAndChairs.js
@@ -7,19 +7,19 @@ const TableAndChairs = () => {
     <div className="layout">
       <div className="square-row top">
         <div className="responsive-square">
-        <button type="button" class="btn btn-outline-secondary">Secondary</button>
+        <button type="button" class="btn btn-outline-secondary">Sit Here 1</button>
         </div>
-        <div className="responsive-square">Square 2</div>
+        <div className="responsive-square">        <button type="button" class="btn btn-outline-secondary">Sit Here 2</button></div>
       </div>
       <div className="rectangle-row">
-        <div className="responsive-square">Square 5</div> 
+        <div className="responsive-square">        <button type="button" class="btn btn-outline-secondary">Sit Here 6</button></div> 
         <div className="responsive-rectangle">
           <RectangleContent /> 
-        </div>        <div className="responsive-square">Square 6</div> 
+        </div>        <div className="responsive-square">        <button type="button" class="btn btn-outline-secondary">Sit Here 3</button></div> 
       </div>
       <div className="square-row bottom">
-        <div className="responsive-square">Square 3</div>
-        <div className="responsive-square">Square 4</div>
+        <div className="responsive-square">        <button type="button" class="btn btn-outline-secondary">Sit Here 5</button></div>
+        <div className="responsive-square">        <button type="button" class="btn btn-outline-secondary">Sit Here 6</button></div>
       </div>
     </div>
   );
