5f90e4b6c769a73b9cf2864bb27c789a08ed798c
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-14 16:01:31 -0400

    pause for sat the 14

--- client/src/components/pages/sampleWork/SampleRoom.js
+++ client/src/components/pages/sampleWork/SampleRoom.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ChildComponent from "./TableAndChairs";
+import TableAndChairs from "./TableAndChairs";
 
 export default function SampleRoom() {
   return (
     <div className="responsive-box">
       <div className="sRoomExit"><button type="button" class="btn-close" aria-label="Close"></button></div>
-      <ChildComponent />
+      <TableAndChairs />
   </div>
   );
 }
