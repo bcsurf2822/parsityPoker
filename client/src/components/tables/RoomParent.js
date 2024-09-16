c08acfc40f0dbcb60a3382eae59b5c415ef1b213
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-16 11:15:48 -0400

    able to add button to new room to test player leaving

--- client/src/components/tables/RoomParent.js
+++ client/src/components/tables/RoomParent.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import NewRoom from './NewRoom'
+import React from "react";
+import NewRoom from "./NewRoom";
+
+import { useDispatch } from "react-redux";
+
+import { startLeaveGame } from "../../rtk/slices/currentGameSlice";
 
 export default function RoomParent() {
+  const dispatch = useDispatch();
+
+  const handleLeaveGame = (userId, gameId) => {
+    console.log("Dispatching startLeaveGame with params:", userId, gameId);
+    dispatch(startLeaveGame({ userId, gameId }));
+  };
+
   return (
     <div className="responsive-box">
-            <div className="sRoomExit"><button type="button" class="btn-close" aria-label="Close"></button></div>
-            <NewRoom />
+      <button type="button" class="btn-close" aria-label="Close"></button>
+
+      <NewRoom />
     </div>
-  )
+  );
 }
