fe352a6bfcfd840f5011434bb4f50334fb6eb37e
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-18 14:27:07 -0400

    worked on navbar a bit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { Navbar, Nav, NavDropdown, Badge } from "react-bootstrap";
 import { logout } from "../rtk/actions/auth";
+import { Badge } from "react-bootstrap";
 
 const MyNav = () => {
   const navigate = useNavigate();
@@ -14,81 +14,89 @@ const MyNav = () => {
     navigate("/");
   };
 
-  const navStyle = {
-    fontFamily: "Lobster, cursive",
-  };
-
-  const customNavStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-  };
-
-  const navLinkStyle = {
-    margin: '0 10px',
-    position: 'relative',
-  };
-
-  const navLinkDividerStyle = {
-    position: 'absolute',
-    right: 0,
-    top: '50%',
-    transform: 'translateY(-50%)',
-    height: '24px',
-    borderRight: '1px solid #ccc',
-  };
-
   return (
-    <Navbar style={navStyle} bg="light" expand="lg" sticky="top">
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-   <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="justify-content-center" style={customNavStyle}>
-          <Nav.Link as={Link} to="/" style={navLinkStyle}>
+    <>
+      <ul className="nav nav-underline">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
             Home
-            <span style={navLinkDividerStyle}></span>
-          </Nav.Link>
-          <Nav.Link as={Link} to="/About"  style={navLinkStyle}>About
-          <span style={navLinkDividerStyle}></span>
-          </Nav.Link>
-          <Nav.Link as={Link} to="/Promotions"  style={navLinkStyle}>Promotions
-          <span style={navLinkDividerStyle}></span>
-          </Nav.Link>
-          <Nav.Link as={Link} to="/Tables"  style={navLinkStyle}>Tables
-          {isAuthenticated && <span style={navLinkDividerStyle}></span>}          </Nav.Link>
-          {isAuthenticated && (
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/Profile">
-                Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item  as={Link} to="/Deposit">
-                Deposit
-              </NavDropdown.Item>
-              <NavDropdown.Item  as={Link} to="/Withdrawl">
-                Withdrawl
-              </NavDropdown.Item>
-              <NavDropdown.Item  as={Link} to="/advancedsettings">
-                Advanced Settings
-              </NavDropdown.Item>
-              <NavDropdown.Item  as={Link} to="/handhistory">
-                Hand History
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/accounthistory">
-                Account History
-              </NavDropdown.Item>
-              <NavDropdown.Item onClick={toLogout}>Log Out</NavDropdown.Item>
-            </NavDropdown>
-          )}
-        </Nav>
-      </Navbar.Collapse>
-      {isAuthenticated && (
-        <Navbar.Text style={{ marginRight: "20px" }}>
-          <Badge pill bg="secondary">
-            {userInfo.username}
-
-          </Badge>
-        </Navbar.Text>
-      )}
-    </Navbar>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/promotions">
+            Promotions
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/tables">
+            Tables
+          </Link>
+        </li>
+        {isAuthenticated && (
+          <>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-expanded="false"
+              >
+                Settings
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link className="dropdown-item" to="/Profile">
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/Deposit">
+                    Deposit
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/Withdrawl">
+                    Withdrawl
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/advancedsettings">
+                    Advanced Settings
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/handhistory">
+                    Hand History
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/accounthistory">
+                    Account History
+                  </Link>
+                </li>
+                <li>
+                  <button className="dropdown-item" onClick={toLogout}>
+                    Log Out
+                  </button>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <Badge pill bg="secondary">
+                {userInfo.username}
+              </Badge>
+            </li>
+          </>
+        )}
+      </ul>
+    </>
   );
 };
+
 export default MyNav;
