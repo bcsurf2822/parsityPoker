527f50f45c8493ebd2b5500efff82b4d86902c0f
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-24 10:57:21 -0400

    uh so frustrating

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,18 +1,18 @@
 const About = () => {
   return (
     <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100 text-center solid-brdr">
-      <div className="row align-items-start tb-seats solid-brdr">
-        <div className="col">One of three columns</div>
-        <div className="col">One of three columns</div>
+      <div className="row align-items-start tb-seats ">
+        <div className="col mx-5  tb-seats">One of three columns</div>
+        <div className="col mx-5  tb-seats">One of three columns</div>
       </div>
-      <div className="row align-items-center solid-brdr mid-row">
-        <div className="col">One of three columns</div>
-        <div className="col bs-table mx-2 solid-brdr">One of three columns</div>
-        <div className="col">One of three columns</div>
+      <div className="row align-items-center mid-row">
+        <div className="col mx-5">One of three columns</div>
+        <div className="col bs-table">One of three columns</div>
+        <div className="col mx-5">One of three columns</div>
       </div>
-      <div className="row align-items-star solid-brdr">
-        <div className="col">One of three columns</div>
-        <div className="col">One of three columns</div>
+      <div className="row align-items-star">
+        <div className="col mx-5 tb-seats">One of three columns</div>
+        <div className="col mx-5 tb-seats">One of three columns</div>
       </div>
     </div>
   );
