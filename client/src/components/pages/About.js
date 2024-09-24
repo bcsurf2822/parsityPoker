05716c9e3ba6fcb6b75d7e9d566bd5af9b4d5ee1
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-24 10:48:35 -0400

    furth exp with about table des

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,16 +1,16 @@
 const About = () => {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100 text-center exp-rm">
-      <div className="row align-items-start tb-seats">
+    <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100 text-center solid-brdr">
+      <div className="row align-items-start tb-seats solid-brdr">
         <div className="col">One of three columns</div>
         <div className="col">One of three columns</div>
       </div>
-      <div className="row align-items-start">
+      <div className="row align-items-center solid-brdr mid-row">
         <div className="col">One of three columns</div>
-        <div className="col">One of three columns</div>
+        <div className="col bs-table mx-2 solid-brdr">One of three columns</div>
         <div className="col">One of three columns</div>
       </div>
-      <div className="row align-items-start">
+      <div className="row align-items-star solid-brdr">
         <div className="col">One of three columns</div>
         <div className="col">One of three columns</div>
       </div>
