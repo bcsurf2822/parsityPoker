18bd4c1bbeb474e72bf0459e78e536a418189aca
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-25 09:53:40 -0400

    trying with exp comp but might bad it

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -2,11 +2,11 @@ const About = () => {
   return (
     <div className="container-fluid d-flex flex-column justify-content-center align-items-center min-vh-100 text-center solid-brdr">
       <div className="row justify-content-around align-items-start">
-        <div className="col-auto tb-seats side-seats">Seat1</div> {/* Left seat */}
-        <div className="col-auto tb-seats side-seats">Seat2</div> {/* Right seat */}
+        <div className="col-auto tb-seats side-seats">Seat1</div> 
+        <div className="col-auto tb-seats side-seats">Seat2</div> 
       </div>
       <div className="row justify-content-around align-items-center mid-row">
-        <div className="col-auto tb-seats side-seats">Seat6</div> {/* Left seat */}
+        <div className="col-auto tb-seats side-seats">Seat6</div>
         <div className="col-auto bs-table">
           <div className="card-row">
             <div className="card-space"></div>
@@ -17,11 +17,11 @@ const About = () => {
           </div>
           <div className="pot-amount">$0.00</div>
         </div>
-        <div className="col-auto tb-seats side-seats">Seat3</div> {/* Right seat */}
+        <div className="col-auto tb-seats side-seats">Seat3</div> 
       </div>
       <div className="row justify-content-around align-items-start">
-        <div className="col-auto tb-seats side-seats">Seat5</div> {/* Left seat */}
-        <div className="col-auto tb-seats side-seats">Seat4</div> {/* Right seat */}
+        <div className="col-auto tb-seats side-seats">Seat5</div> 
+        <div className="col-auto tb-seats side-seats">Seat4</div> 
       </div>
     </div>
   );
