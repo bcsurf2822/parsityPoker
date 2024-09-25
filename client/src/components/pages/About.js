e230ede4965ce2bd8d9335a07274f633ac0c8cea
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-25 08:51:05 -0400

    furth experiment with bootstrap grid sysstem for new room

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,18 +1,27 @@
 const About = () => {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100 text-center solid-brdr">
-      <div className="row align-items-start tb-seats ">
-        <div className="col mx-5  tb-seats">One of three columns</div>
-        <div className="col mx-5  tb-seats">One of three columns</div>
+    <div className="container-fluid d-flex flex-column justify-content-center align-items-center min-vh-100 text-center solid-brdr">
+      <div className="row justify-content-around align-items-start">
+        <div className="col-auto tb-seats">Seat</div>
+        <div className="col-auto tb-seats">Seat</div>
       </div>
-      <div className="row align-items-center mid-row">
-        <div className="col mx-5">One of three columns</div>
-        <div className="col bs-table">One of three columns</div>
-        <div className="col mx-5">One of three columns</div>
+      <div className="row justify-content-around align-items-center mid-row">
+        <div className="col-auto tb-seats">Seat</div>
+        <div className="col-auto bs-table">
+          <div className="row card-row">
+            <div className="col-auto card-space"></div>
+            <div className="col-auto card-space"></div>
+            <div className="col-auto card-space"></div>
+            <div className="col-auto card-space"></div>
+            <div className="col-auto card-space"></div>
+          </div>
+          <div className="row align-items-center justify-content-center pot-amount">$0.00</div>
+        </div>
+        <div className="col-auto tb-seats">Seat</div>
       </div>
-      <div className="row align-items-star">
-        <div className="col mx-5 tb-seats">One of three columns</div>
-        <div className="col mx-5 tb-seats">One of three columns</div>
+      <div className="row justify-content-around align-items-start">
+        <div className="col-auto tb-seats">Seat</div>
+        <div className="col-auto tb-seats">Seat</div>
       </div>
     </div>
   );
