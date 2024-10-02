2bef09bdc5d9d229e5b8fc3d0324a2a3a34616fb
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-10-02 11:37:33 -0400

    table design is still coming along

--- client/src/components/ExperimentalRoom/Exp.js
+++ client/src/components/ExperimentalRoom/Exp.js
@@ -1,24 +1,43 @@
 import React from "react";
 import "./Exp.css";
 import { Button } from "react-bootstrap";
+import { getCardColor } from "../../actions/getCardColor";
+import BetBox from "../tables/BetBox";
 
 export default function ExperimentalRoom() {
   return (
     <div className="container">
       <div className="top-row">
         <Button id="sit-btn">Sit Here</Button>
-        <Button  id="sit-btn">Sit Here</Button>
+        <Button id="sit-btn">Sit Here</Button>
       </div>
       <div className="mid-row">
-      <Button  id="sit-btn">Sit Here</Button>
-      <div className="table">
-        <p>$</p>
-      </div>
-        <Button  id="sit-btn">Sit Here</Button>
+        <Button id="sit-btn">Sit Here</Button>
+        <div className="table">
+
+            {Array.from({ length: 5 }).map((_, index) => (
+              <div className="blue-card" key={index}>
+                <img
+                  src={getCardColor()}
+                  alt={`Card ${index + 1}`}
+                  className="card-image"
+                />
+              </div>
+            ))}
+    
+        </div>
+        <Button id="sit-btn">Sit Here</Button>
       </div>
       <div className="bot-row">
-        <Button  id="sit-btn">Sit Here</Button>
-        <Button  id="sit-btn">Sit Here</Button>
+        <div className="bot-btns">
+            <Button id="sit-btn">Sit Here</Button>
+        <Button id="sit-btn">Sit Here</Button>
+        </div>
+      
+
+        {/* <div className="bet-box">
+            <Button />
+      </div> */}
       </div>
     </div>
   );
